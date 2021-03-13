59dc03ade78bd19537eb34c01fbcbc0cd3992086
Owen Young <theowenyoung@gmail.com>
2021-03-14 07:43:00 +0800

    feat: add google anylyise

--- gatsby-config.js
+++ gatsby-config.js
@@ -124,5 +124,14 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-6Y58K64XXC", // Google Analytics / GA
+        ],
+      },
+    },
   ],
 }
